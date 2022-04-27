9db6ffbbb4326b5d4b1955332e20da6341f328d3
Lucas Rufino Martelotte <45052925+lucas-martelotte@users.noreply.github.com>
2022-04-26 23:41:14 -0300

    add comment

--- client.py
+++ client.py
@@ -11,11 +11,13 @@ while True:
     print('Digite o nome do arquivo (não digite nada para sair):')
     filename = str(input())
     if filename == '':
+        # Caso a entrada esteja vazia, vamos encerrar o programa
         exit()
     else:
         client.sendall(pickle.dumps(filename))
         data = pickle.loads(client.recv(1024))
         if data == []:
+            # Se for retornada uma lista vazia, é porque o usuário mandou um nome de arquivo inválido
             print('Entrada invalida. Tente Novamente.')
         else:
             print(f'As palavras mais frequentes do arquivo \"{filename}.txt\" são {data}')
